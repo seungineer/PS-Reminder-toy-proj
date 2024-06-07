cfee8bf8f2c4f3275271b53c8c297783e574a5c6
조승우 <choseung97@gmail.com>
2024-06-07 14:42:20 +0900

    chore(config): tailwind config fix, Button.js 삭제

--- week-1/tailwind.config.js
+++ week-1/tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "False",
-  content: ["./src/**/*.{js, jsx, ts, tsx}"],
+  darkMode: "false",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
