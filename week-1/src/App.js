23a2940db4f379d9465efbbddfba3503ab60d679
조승우 <choseung97@gmail.com>
2024-06-09 21:44:43 +0900

    fix: 누락된 styled-components 추가

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Router from "./shared/Router";
 // styled-components에서 styled 라는 키워드를 import 합니다.
-import styled from "styled-components";
+// import styled from "styled-components";
 import GlobalStyle from "./GlobalStyle";
 import {RecoilRoot} from 'recoil';
 
