c67ab98f73c2738a4269eaacb966ca8e1e9b828e
조승우 <choseung97@gmail.com>
2024-06-11 03:25:53 +0900

    feat(post 영역): 항목(카테고리, 문제 제목, 문제 링크, 점수 등) 추가

--- week-1/src/pages/WritePost.js
+++ week-1/src/pages/WritePost.js
@@ -1,14 +1,22 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";      
 import { isRefreshState, postsState, tempdataState } from "../shared/Store";
 import { useNavigate } from "react-router-dom";
 import { SlArrowLeftCircle } from "react-icons/sl";
 import axios from "axios";
+import Rating from 'react-rating';
+
 
 const WritePost = () => {
     // subject, content 상태값 추가
     const [subject, setSubject] = useState("");
     const [content, setContent] = useState("");
+    const [category, setCategory] = useState("Leet Code");
+    const [link, setLink] = useState("");
+    const [titlePlaceholder, setTitlePlaceholder] = useState("");
+    const [linkPlaceholder, setLinkPlaceholder] = useState("");
+    const [score, setScore] = useState(0);
+
     const idRef = useRef(useRecoilValue(postsState).length + 1);
     
     const [posts, setPosts] = useRecoilState(postsState);
@@ -29,9 +37,9 @@ const WritePost = () => {
         const newServerPost = {
             "title":subject,
             "content":content,
-            "link":"test",
-            "category":"test",
-            "score":0,
+            "link":link,
+            "category":category,
+            "score":score,
             "author":"test",
             "password":"test" 
             };
@@ -61,6 +69,34 @@ const WritePost = () => {
         setTempdata({"subject": subject, "content": content});
     }
 
+    const onLinkChangeHandler = (e) => {
+        setLink(e.target.value);
+    }
+
+    const handleRadioChange = (event) => {
+        setCategory(event.target.value);
+        };
+    const onScoreChangeHandler = (value) => {
+        setScore(value);
+    }
+
+    useEffect(() => {
+        // category 값이 변경될 때마다 placeholder 업데이트
+        switch (category) {
+          case 'Leet Code':
+            setTitlePlaceholder('ex) 1. Two Sum');
+            setLinkPlaceholder('ex) leetcode.com/problems/two-sum/');
+            break;
+          case '백준':
+            setTitlePlaceholder('ex) 1000. A+B');
+            setLinkPlaceholder('ex) acmicpc.net/problem/1000');
+            break;
+          default:
+            setTitlePlaceholder('ex) 1. Two Sum');
+            setLinkPlaceholder('ex) leetcode.com/problems/two-sum/');
+        }
+          }, [category]); // category가 변경될 때만 useEffect 실행
+
     return (
         <> 
         <button onClick={()=>navigate('/Home')} className="mb-3">
@@ -74,12 +110,54 @@ const WritePost = () => {
                     padding: "5px",
                 }}
             >
+                <div className="mb-4">
+                <div>
+                    <label
+                        htmlFor="카테고리"
+                        className="block mb-3 text-sm font-semibold text-gray-900 dark:text-white"
+                    >
+                        Category
+                    </label>
+                </div>
+                {/* Category Radio Section */}
+                <div>
+                <div class="flex items-center mb-2">
+                    <input checked={category ==='Leet Code'} id="default-radio-1" type="radio" value="Leet Code" name="default-radio" onChange={handleRadioChange} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                    <label checked={category ==='백준'} for="default-radio-1" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Leet Code</label>
+                </div>
+                <div class="flex items-center mb-2">
+                    <input id="default-radio-2" type="radio" value="백준" name="default-radio" onChange={handleRadioChange} class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                    <label for="default-radio-2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">백준</label>
+                </div>
+                </div>
+                </div>
+                <div className="mb-4">
+                    <div>
+                    <label
+                        htmlFor="message"
+                        className="block mb-0.5 text-sm font-semibold text-gray-900 dark:text-white"
+                    >
+                        How much you want to solve it again?
+                    </label>
+                    </div>
+                    <div>
+                    <Rating
+                        start={0}
+                        stop={10}
+                        step={1}
+                        initialRating={6}
+                        onChange={onScoreChangeHandler}
+                    />
+                    </div>
+                </div>
+                
+                {/* Subjects 입력 Section */}
                 <div>
                     <label
                         htmlFor="message"
-                        className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white"
+                        className="block mb-0.5 text-sm font-semibold text-gray-900 dark:text-white"
                     >
-                        Subjects
+                        Problem Title
                     </label>
                 </div>
                 <div>
@@ -87,15 +165,37 @@ const WritePost = () => {
                         id="message"
                         onChange={onSubjectChangeHandler}
                         rows="1"
-                        className="block p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Writes the Subjects Here."
+                        className="block mb-4 p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder={titlePlaceholder}
                         defaultValue={tempdata.subject}
                     ></textarea>
                 </div>
+
+                {/* Link 입력 Section */}
                 <div>
                     <label
                         htmlFor="message"
-                        className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white"
+                        className="block mb-0.5 text-sm font-semibold text-gray-900 dark:text-white"
+                    >
+                        Problem Link
+                    </label>
+                </div>
+                <div>
+                    <textarea
+                        id="message"
+                        onChange={onLinkChangeHandler}
+                        rows="1"
+                        className="block mb-4 p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder={linkPlaceholder}
+                        defaultValue={tempdata.subject}
+                    ></textarea>
+                </div>
+
+                {/* Contents 입력 Section */}
+                <div>
+                    <label
+                        htmlFor="message"
+                        className="block mb-0.5 text-sm font-semibold text-gray-900 dark:text-white"
                     >
                         Contents
                     </label>
