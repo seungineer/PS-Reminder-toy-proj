6fff8ab687f03e428c9348352baa9d6b3ba4bc86
조승우 <choseung97@gmail.com>
2024-06-10 14:17:26 +0900

    feat(call api): 로그인을 위한 post 요청

--- week-1/src/pages/Home.js
+++ week-1/src/pages/Home.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BulletBoard from "../components/BulletBoard";
 import { useRecoilState } from "recoil";
 import { postsState } from "../shared/Store";
 import { useNavigate } from "react-router-dom";
 import { SlPlus } from "react-icons/sl";
+import axios from 'axios';
 
 const Home = () => {
     // subject, content 상태값 추가
     const [posts, setPosts] = useRecoilState(postsState)
     const navigate = useNavigate();
-
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      axios.get('http://43.201.17.0:8080/')
+        .then(response => {
+          setData(response.data);
+        })
+        .catch(error => {
+          setError(error);
+        });
+    }, []);
+  
+    
     return (
         <>
+            {/* axios test */}
+            {error && <div>Error: {error.message}</div>}
+            {(!data) && <div>Loading...</div>}
+            <div className="flex w-full flex-wrap justify-center gap-4 p-4">
+                {data.length > 0 ? (
+                    data.map((post) => <div key={post.id}>{post.title}</div>)
+                ) : (
+                    <div>게시물이 없습니다.</div>
+                )}
+            </div>
             {/* card 영역 */}
             <div className="flex w-full flex-wrap justify-center gap-4 p-4">
                 {posts.length > 0 ? (
--- week-1/src/pages/Login.js
+++ week-1/src/pages/Login.js
@@ -1,39 +1,40 @@
-import React, { useRef, useState } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";      
-import { postsState } from "../shared/Store";
+import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SlArrowLeftCircle } from "react-icons/sl";
 
-const WritePost = () => {
-    // subject, content 상태값 추가
-    const [subject, setSubject] = useState("");
-    const [content, setContent] = useState("");
-    const [posts, setPosts] = useRecoilState(postsState)
-    const idRef = useRef(useRecoilValue(postsState).length + 1);
+const Login = () => {
+    // id, password 상태값 추가
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    const onSubjectChangeHandler = (e) => {
-        setSubject(e.target.value);
+    const onIdChangeHandler = (e) => {
+        setId(e.target.value);
     };
-    const onContentChangeHandler = (e) => {
-        setContent(e.target.value);
+    const onPasswordChangeHandler = (e) => {
+        setPassword(e.target.value);
     };
 
-    const onPostClick = () => {
-        console.log("post button clicked!");
-        const newPost = {
-            id: idRef.current,
-            subject,
-            content,
-        };
-        setPosts([...posts, newPost]);
-        idRef.current += 1;
-        navigate("/Home");
+    const onLoginClick = () => {
+        // 서버에 id와 password 전송
+        axios.post("http://43.201.17.0:8080/api/auth/login", {
+            "username" : id,
+            "password" : password,
+        })
+            .then((data) => {
+            // 토큰 response 처리
+            const token = data.token;
+            // 토큰을 localStorage에 저장
+            localStorage.setItem("token", token);
+            // 홈 페이지로 이동
+            navigate("/Home");
+            })
+            .catch((error) => {
+            navigate("/Home");
+            console.error("Error:", error);
+            });
     };
     return (
-        <> 
-        <button onClick={()=>navigate('/Home')} className="mb-3">
-            <SlArrowLeftCircle className = "" size={40}/>
-        </button>
+        <>
         <div
                 style={{
                     width: "100%",
@@ -44,19 +45,19 @@ const WritePost = () => {
             >
                 <div>
                     <label
-                        htmlFor="message"
+                        htmlFor="아이디"
                         className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                        Subjects
+                        아이디
                     </label>
                 </div>
                 <div>
                     <textarea
-                        id="message"
-                        onChange={onSubjectChangeHandler}
+                        id="패스워드"
+                        onChange={onIdChangeHandler}
                         rows="1"
                         className="block p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Writes the Subjects Here."
+                        placeholder="Writes the ids Here."
                     ></textarea>
                 </div>
                 <div>
@@ -64,32 +65,32 @@ const WritePost = () => {
                         htmlFor="message"
                         className="block mb-0.5 text-sm font-medium text-gray-900 dark:text-white"
                     >
-                        Contents
+                        패스워드
                     </label>
                 </div>
                 <div>
                     <textarea
                         id="message"
-                        onChange={onContentChangeHandler}
-                        rows="4"
+                        onChange={onPasswordChangeHandler}
+                        rows="1"
                         className="block mb-1.5 p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Writes the Contents Here."
+                        placeholder="Writes the passwords Here."
                     ></textarea>
                 </div>
-                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                <div style={{ display: "flex", justify: "center" }}>
                     <button
                         type="button"
-                        onClick={onPostClick}
+                        onClick={onLoginClick}
                         className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium  text-sm px-4 py-1.2  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800
                 disabled:bg-gray-400 disabled:cursor-not-allowed"
-                        disabled={subject.length < 10}
+                        disabled={ password.length < 3}
                         style={{ borderRadius: "4px" }}
                     >
-                        Post
+                        로그인
                     </button>
                 </div>
             </div>
         </>
     );
 };
-export default WritePost;
+export default Login;
